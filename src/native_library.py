13f61201f4f070900c6290cb25dcf10ae01fdbf1
Tristam MacDonald <tristam@trist.am>
2024-04-25 10:12:13 +0200

    fix dll name on windows

--- src/native_library.py
+++ src/native_library.py
@@ -5,7 +5,7 @@ import platform
 
 def native_library(directory, name):
     if platform.system() == 'Windows':
-        return path.join(directory, 'Debug', '{name}.dll')
+        return path.join(directory, 'Debug', f'{name}.dll')
     elif platform.system() == 'Darwin':
         return path.join(directory, f'lib{name}.dylib')
     else:
